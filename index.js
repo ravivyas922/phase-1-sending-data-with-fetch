c2620839ee5b74fa95054a0121a58ebd6e8ad12f
Ravi <ravivyas922@gmail.com>
2023-01-05 13:06:57 -0500

    commit tree bdbb02cea1d99a4577961ef9a5ae8e4433f4e300

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your code here
+const data= {
+
+}
+function submitData(name,email)
+{
+    data[0] = name; 
+    data[1] = email;
+    return fetch('http://localhost:3000/users',{
+        method: 'POST',
+        headers: 
+        {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then((response) => response.json())
+    .then((data) => {console.log(data);})
+}
